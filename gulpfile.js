91c171e8114f253051972de3dcde3fd90e5f2628
shinMH <lunaoflight09@gmail.com>
2025-01-07 22:51:56 +0900

    gulp setting

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+var gulp = require("gulp");
+var fileinclude = require("gulp-file-include");
+var scss = require("gulp-sass")(require("sass"));
+var clean = require("gulp-clean");
+var browserSync = require("browser-sync").create();
+var sourcemaps = require("gulp-sourcemaps");
+
+var paths = {
+  dev: {
+    src: "./src",
+    css: "./src/**/css/*.scss",
+    js: "./src/**/js/*.js",
+    html: ["./src/**/*.html", "!./src/site/public/include/*.html"],
+    resource: [
+      "./src/**/*",
+      "!./src/**/*.js",
+      "!./src/**/*.css",
+      "!./src/**/*.map",
+      "!./src/**/*.scss",
+      "!./src/**/*.html",
+    ],
+  },
+  dist: {
+    server: "./docs/2025_fila",
+    src: "./docs",
+    css: "./docs",
+    js: "./docs",
+    html: "./docs",
+    resource: "./docs",
+  },
+};
+
+var scssOptions = {
+  outputStyle: "expanded",
+  indentType: "tab",
+  indentWidth: 2,
+  precision: 6,
+  sourceComments: false,
+};
+
+function setHtml() {
+  return gulp
+    .src(paths.dev.html)
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        surffix: "##",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest(paths.dist.html))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function setJs() {
+  return gulp
+    .src(paths.dev.js)
+    .pipe(gulp.dest(paths.dist.js))
+    .pipe(browserSync.reload({ stream: true }));
+}
+function setCss() {
+  return gulp
+    .src(paths.dev.css)
+    .pipe(sourcemaps.init())
+    .pipe(scss(scssOptions).on("error", scss.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.src("./src/scss/*.css"), { passthrough: true })
+    .pipe(gulp.dest(paths.dist.css))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function setResource() {
+  return gulp
+    .src(paths.dev.resource)
+    .pipe(gulp.dest(paths.dist.resource))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function watchFiles() {
+  gulp.watch(paths.dev.css, setCss);
+  gulp.watch(paths.dev.js, setJs);
+  gulp.watch(paths.dev.html, setHtml);
+  gulp.watch(paths.dev.resource, setResource);
+}
+function brwSync() {
+  return browserSync.init({
+    server: {
+      baseDir: paths.dist.src,
+    },
+    port: 8000,
+  });
+}
+function setClean() {
+  return gulp
+    .src(paths.dist.src, { read: false, allowEmpty: true })
+    .pipe(clean());
+}
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    brwSync,
+    gulp.series(setClean, setHtml, setCss, setJs, setResource),
+    watchFiles
+  )
+);
---
+++ readme.md
@@ -0,0 +1,5 @@
+#yarn dev
+
+#root 경로 : /docs
+
+#server : https://fine-today.github.io/2025_fila 접속
