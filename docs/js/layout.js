fcd1cab0d025d490a6ee1c00ef2ab48fbd39416c
finetonight <finetonight@gmail.com>
2025-02-26 09:10:38 +0900

    merge

---
+++ docs/js/layout.js
@@ -0,0 +1,494 @@
+var $logo = `
+  <svg width="90" height="17" viewBox="0 0 90 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2301_103823)">
+<path d="M89.9255 5.95741C89.7996 4.66006 89.6138 3.74234 88.9981 2.84919C88.3844 1.95799 87.5108 1.25951 86.4022 0.773813C85.3007 0.291993 84.3284 0.0727491 82.5749 0.0475263C81.4558 0.0313579 80.3375 0.0307111 79.2184 0.0475263C77.4649 0.0727491 76.4926 0.291993 75.3911 0.773813C74.2818 1.25951 73.4083 1.95799 72.7952 2.84919C72.1801 3.74234 71.9944 4.66071 71.8677 5.95741C71.7418 7.25283 71.7983 8.83669 71.8677 10.1638C71.9359 11.4599 72.1795 12.3795 72.7952 13.272C73.4089 14.1639 74.2825 14.8617 75.3918 15.3474C76.492 15.8292 77.9929 16.0258 79.2184 16.0737C80.3349 16.1177 81.4597 16.1177 82.5749 16.0737C83.8004 16.0258 85.3013 15.8292 86.4015 15.3474C87.5108 14.8623 88.3844 14.1639 88.9981 13.272C89.6132 12.3795 89.8574 11.4599 89.9255 10.1638C89.9957 8.83669 90.0515 7.25283 89.9255 5.95741ZM86.2801 9.34567C86.2145 12.0671 84.7188 13.1724 82.0845 13.2002C81.6883 13.2035 81.2928 13.206 80.8966 13.206C80.5005 13.206 80.1049 13.2035 79.7088 13.2002C77.0745 13.1724 75.5788 12.0671 75.5132 9.34567C75.508 8.9434 75.4827 8.49715 75.4827 8.07095C75.4827 7.64475 75.508 7.1985 75.5132 6.79623C75.5788 4.07476 77.0745 2.96949 79.7088 2.94168C80.1049 2.93844 80.5005 2.93586 80.8966 2.93586C81.2928 2.93586 81.6883 2.93844 82.0845 2.94168C84.7188 2.96949 86.2145 4.07476 86.2801 6.79623C86.2853 7.1985 86.3106 7.64475 86.3106 8.07095C86.3106 8.49715 86.2853 8.9434 86.2801 9.34567Z" fill="black"/>
+<path d="M54.1597 3.13634H60.2725V15.8997H60.2945H63.9374H63.9595V3.13634H70.0729V0.242188H54.1597V3.13634Z" fill="black"/>
+<path d="M44.8251 6.50318C40.9387 6.04594 39.8749 5.676 39.8749 4.45625C39.8749 3.30183 41.0478 2.93513 43.8211 2.93513C47.1801 2.93513 48.2271 3.60191 48.1887 4.6949L48.192 4.93549H51.8069V4.65222C51.8069 1.45345 49.8754 0.0920692 44.2958 0.0370965C44.177 0.035803 44.0568 0.0351562 43.9347 0.0351562C38.7162 0.0351562 36.2574 1.4942 36.2574 4.65222C36.2574 7.81024 38.2097 8.72472 43.3846 9.35594C47.4243 9.83517 48.5505 10.1779 48.5505 11.3828C48.5505 12.5877 47.3438 13.2092 44.6141 13.2092C40.7302 13.2092 39.5768 12.6718 39.5378 11.247L39.5287 10.9896H35.8742L35.8716 11.4274C35.9554 15.3318 38.6558 16.1072 44.0321 16.1072H45.2616C49.9514 16.1072 52.1927 14.4768 52.1927 11.6066V11.3207C52.1927 8.17694 50.1313 7.13504 44.8251 6.50318Z" fill="black"/>
+<path d="M31.9693 0.242188H28.2822V15.8991H31.9693V0.242188Z" fill="black"/>
+<path d="M17.9526 0.0351562H17.5941C15.141 0.0746072 13.822 0.806067 12.7971 1.84602C12.36 1.34157 11.1754 0.0351562 8.31643 0.0351562H7.96118C6.5843 0.0590855 4.79567 0.287384 3.61689 1.40042V0.244699H0V15.899H3.61494L3.61624 5.46193C3.62598 4.03781 4.98012 3.15437 6.91748 3.15437C6.98438 3.15437 7.05127 3.15178 7.11817 3.15437C8.66 3.21516 10.0427 3.68664 10.0635 5.63202V15.899H13.6784V5.48327C13.6888 4.04493 15.093 3.15437 17.0407 3.15437H17.2414C19.0827 3.15437 20.2718 4.04363 20.2725 5.63202V15.899H23.8874V4.70525C23.8394 1.74578 21.8345 0.0351562 17.9526 0.0351562Z" fill="black"/>
+</g>
+<defs>
+<clipPath id="clip0_2301_103823">
+<rect width="90" height="16.0714" fill="white" transform="translate(0 0.0351562)"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+$(function () {
+  var $document = $(document);
+  var $html = $("html");
+  var $body = $("body");
+  var $window = $(window);
+  // MENU List
+  var MENU = [
+    {
+      id: "0",
+      title: "Misto",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "We Are Misto",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "연혁",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "브랜드",
+          anchor: "#n",
+          active: true,
+        },
+      ],
+    },
+    {
+      id: "1",
+      title: "Investors",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "재무정보",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "공시정보",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "주주총회",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "주주환원",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "IR자료",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "기업지배구조",
+          anchor: "#n",
+          active: false,
+        },
+      ],
+    },
+    {
+      id: "2",
+      title: "Sustainability   ",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "ESG Highlights",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "ESG 평가정보",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "지속가능경영보고서",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "ESG 정책",
+          anchor: "#n",
+          active: false,
+        },
+      ],
+    },
+    { id: "logo", title: "logo", anchor: "#n", active: false },
+    {
+      id: "3",
+      title: "Careers",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "문화",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "인사제도와 복지",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "채용공고",
+          anchor: "#n",
+          active: false,
+        },
+      ],
+    },
+    {
+      id: "4",
+      title: "Newsroom",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "보도자료",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "아카이브",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-3",
+          title: "CI / BI",
+          anchor: "#n",
+          active: false,
+        },
+      ],
+    },
+    {
+      id: "5",
+      title: "Contact",
+      anchor: "#n",
+      active: false,
+      sub: [
+        {
+          id: "0-1",
+          title: "제보채널",
+          anchor: "#n",
+          active: false,
+        },
+        {
+          id: "0-2",
+          title: "오시는길",
+          anchor: "#n",
+          active: false,
+        },
+      ],
+    },
+  ];
+
+  //menu 생성
+  var $menu = $("#menu");
+  var $menuList = $menu.find(">.depth-list");
+  var $footerMenuList = $("#footer-menu > .depth-list");
+  MENU.forEach(function (elem, i) {
+    var { id, title, anchor, sub, active } = elem;
+    var $depth1 = $(`
+      <li class="depth-item depth1-item ${id === "logo" ? "logo" : ""}">
+        
+          ${
+            id === "logo"
+              ? `<a href="/" class="logo-anchor">${$logo}</a>`
+              : `
+                <a class="depth-anchor depth1-anchor" href="${anchor}">
+                  <span>${title}</span>
+                </a>
+              `
+          }
+        </a>
+      </li>`);
+    if (!!sub) {
+      $depth1.addClass("has");
+      var activeFlag = false;
+      var $depth2 = $(`<div class="depth depth2"></div>`);
+      var $depth2List = $(`<ul class="depth-list depth2-list"></ul>`);
+      sub.forEach(function (subElem, index) {
+        var { id, title, anchor, active } = subElem;
+        var $depth2 = $(`
+          <li class="depth-item depth2-item${active ? " actived" : ""}">
+            <a class="depth-anchor depth2-anchor" href="${anchor}" ${`title="${
+          active ? "현재 위치" : ""
+        }"`}>
+              <span>${title}</span>
+            </a>
+          </li>`);
+        if (active) activeFlag = true;
+        $depth2List.append($depth2);
+      });
+      if (activeFlag) $depth1.addClass("actived");
+      $depth2.append($depth2List);
+      $depth1.append($depth2);
+    }
+    var cloneDepth1 = $depth1.clone();
+    $menuList.append($depth1);
+    $footerMenuList.append(cloneDepth1);
+
+    //footer menu-height
+    const setFooterHeight = () => {
+      var $depth1 = $footerMenuList.find(".depth1-item");
+      var menuHeight = 0;
+      $depth1.each(function () {
+        var $this = $(this);
+        var $thisAnchor = $this.find(">.depth1-anchor");
+        if ($this.hasClass("has")) {
+          var depth2Height = $this.find(".depth2").outerHeight();
+          if (menuHeight < depth2Height) {
+            menuHeight = depth2Height + 47;
+          }
+        }
+      });
+      $footerMenuList.height(menuHeight);
+    };
+    setFooterHeight();
+  });
+
+  // mouseover event
+  $document.on("mouseover focusin", "#menu", function () {
+    if ($window.outerWidth() > 1200) {
+      $html.attr("data-menu-open", "true");
+      // 높이값 저장
+      var depth2Height = 0;
+      $(".depth2").each(function () {
+        var $this = $(this);
+        var thisHeight = $this.outerHeight();
+        if (depth2Height < thisHeight) {
+          depth2Height = thisHeight;
+        }
+      });
+      var headerHeight = $(".header").outerHeight();
+      $(".header .menu-wrap").height(headerHeight + depth2Height);
+    }
+  });
+
+  $document.on("mouseover focusin", "#menu .depth-item", function () {
+    if ($window.outerWidth() > 1200) {
+      var $this = $(this),
+        $thisAnchor = $this.find(">.depth-anchor");
+      var $headerMenu = $(".header .depth1"),
+        $headerMenuItem = $headerMenu.find(".depth-item");
+      $(".depth2").removeAttr("style");
+      $headerMenu.find(".actived > .depth-anchor").removeAttr("title");
+      // $headerMenuItem.removeClass("active");
+      $this.addClass("active");
+      $this.parents(".depth-item").addClass("active");
+      $this.siblings().removeClass("active");
+      $headerMenuItem.each(function (i, item) {
+        var $item = $(item),
+          $thisItemList = $item.find(">.depth-list");
+        if ($item.hasClass("has") && $item.hasClass("active")) {
+          // $thisItemList.css("display", "flex");
+          $item.find(">.depth-anchor").attr("title", "열림");
+        } else {
+          $thisItemList.removeAttr("title");
+        }
+      });
+    }
+  });
+
+  $document.on("mouseleave focusout", "#menu", function () {
+    if ($window.outerWidth() > 1200) {
+      $html.attr("data-menu-open", "false");
+      var headerHeight = $(".header").outerHeight();
+      $(".header .menu-wrap").height(headerHeight);
+    }
+  });
+
+  // 반응형 클릭 설정
+  $document.on("click", "#menu .depth-anchor", function (e) {
+    var $this = $(this),
+      $thisItem = $this.closest(".depth-item"),
+      $thisDepth1Item = $this.closest(".depth1-item"),
+      $thisList = $this.closest(".depth-list"),
+      $slblingItem = $thisList.find(">.depth-item"),
+      openFlag = false;
+    if ($window.outerWidth() < 1201) {
+      if ($thisItem.hasClass("has")) {
+        e.preventDefault();
+        $("#menu .depth-item").removeClass("active actived");
+        $thisItem.toggleClass("active");
+        $thisDepth1Item.addClass("active");
+        $thisDepth1Item.siblings().removeClass("active actived");
+      }
+      $slblingItem.each(function () {
+        if ($(this).hasClass("active")) {
+          $(this).find(">.depth").slideDown(200);
+          openFlag = true;
+        } else {
+          $(this).find(">.depth").slideUp(200);
+        }
+      });
+      if (openFlag) {
+        $menu.addClass("all-close");
+      } else {
+        $menu.removeClass("all-close");
+      }
+    }
+  });
+
+  // 메뉴 오픈(반응형 시)
+  var $menuBtn = $(".header .menu-btn");
+  $menuBtn.on("click", function () {
+    var $this = $(this);
+    var $closeIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2377_130779)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.1169 11.6727L3.82324 17.9664L5.23745 19.3806L11.5311 13.0869L17.9653 19.5212L19.3796 18.107L12.9453 11.6727L19.3796 5.23843L17.9653 3.82422L11.5311 10.2585L5.23745 3.96484L3.82324 5.37906L10.1169 11.6727Z" fill="black"/>
+</g>
+<defs>
+<clipPath id="clip0_2377_130779">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>`;
+    var $openIcon = `<svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_2006_83360)">
+        <rect width="20" height="2" transform="translate(2 6)" fill="black" />
+        <rect width="20" height="2" transform="translate(2 16)" fill="black" />
+      </g>
+      <defs>
+        <clipPath id="clip0_2006_83360">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>`;
+    $this.toggleClass("active");
+    if ($this.hasClass("active")) {
+      $html.attr("data-menu-open", "true");
+      $this.attr("title", "메뉴 열림");
+      $this.find("i").html($closeIcon);
+      $menu.addClass("delay");
+      setTimeout(() => {
+        $menu.removeClass("delay");
+      }, 300);
+    } else {
+      $html.attr("data-menu-open", "false");
+      $this.removeAttr("title");
+      $this.find("i").html($openIcon);
+    }
+  });
+
+  //logo focus 문제해결
+  $(".header > .logo").on("focusin", function () {
+    $(".depth1 .logo > a").attr("tab-index", 0).focus();
+  });
+  $document.on("keydown", function (e) {
+    if (e.key === "Tab") {
+      var $focusElem = $(":focus");
+      if ($focusElem.hasClass("logo-anchor") || $focusElem.hasClass("logo")) {
+        $focusElem.attr("tabindex", -1);
+        setTimeout(() => {
+          $(
+            ".depth1 .depth1-list:first-child .depth1-item:first-child >.depth1-anchor:first-child"
+          ).focus();
+        }, 0);
+      }
+    }
+  });
+
+  $window.on("resize", function () {
+    $menuBtn.removeClass("active").removeAttr("title");
+    $html.removeAttr("data-menu-open");
+    $menu.find(".depth-item").removeClass("active");
+    $menu.find(".depth2").css("display", "none");
+  });
+
+  //footer 반응형
+  const setDefault = () => {
+    if ($window.outerWidth() < 1201) {
+      $("#footer-menu .depth2").hide();
+    } else {
+      $("#footer-menu .depth2").css("display", "flex");
+    }
+  };
+  setDefault();
+  $window.on("resize", function () {
+    setDefault();
+  });
+  $document.on("click", "#footer-menu .depth-anchor", function (e) {
+    var $this = $(this),
+      $thisItem = $this.closest(".depth-item"),
+      $thisList = $this.closest(".depth-list"),
+      $slblingItem = $thisList.find(">.depth-item"),
+      openFlag = false;
+    if ($window.outerWidth() < 1201) {
+      if ($thisItem.hasClass("has")) {
+        e.preventDefault();
+        $thisItem.toggleClass("active");
+        $thisItem.siblings().removeClass("active actived");
+      }
+      $slblingItem.each(function () {
+        if ($(this).hasClass("active")) {
+          $(this).find(">.depth").slideDown(200);
+          openFlag = true;
+        } else {
+          $(this).find(">.depth").slideUp(200);
+        }
+      });
+      if (openFlag) {
+        $menu.removeClass("all-close");
+      } else {
+        $menu.addClass("all-close");
+      }
+    }
+  });
+
+  // FAQ board
+  setTimeout(function () {
+    $(".acc-board-item").each(function () {
+      var $this = $(this).find(".acc-title-btn"),
+        $thisItem = $(this),
+        $thisCon = $thisItem.find(".acc-con"),
+        $thisConBox = $thisCon.find(".acc-con-box"),
+        thisConHeight = $thisConBox.outerHeight();
+      if ($thisItem.hasClass("active")) {
+        $thisCon.outerHeight(thisConHeight);
+        $thisCon.attr("title", $this.text() + " 열림");
+      } else {
+        $thisCon.outerHeight(0);
+        $thisCon.attr("title", $this.text());
+      }
+    });
+  }, 0);
+  $(".acc-title-btn").on("click", function () {
+    var $this = $(this),
+      $thisItem = $this.closest(".acc-board-item"),
+      $thisCon = $thisItem.find(".acc-con"),
+      $thisConBox = $thisCon.find(".acc-con-box"),
+      thisConHeight = $thisConBox.outerHeight();
+    $thisItem.toggleClass("active");
+    if ($thisItem.hasClass("active")) {
+      $thisCon.outerHeight(thisConHeight);
+      $thisCon.attr("title", $this.text() + " 열림");
+    } else {
+      $thisCon.outerHeight(0);
+      $thisCon.attr("title", $this.text());
+    }
+  });
+});
---
+++ docs/js/sub.js
@@ -0,0 +1,290 @@
+"use strict";
+
+$(function () {
+  var $window = $(window),
+    $html = $("html"),
+    $container = $("#container"),
+    $footer = $("#footer");
+
+  //레이어팝업
+  $("[data-pop]:not('.popup')").on("click", function () {
+    var $this = $(this),
+      thisId = $this.attr("data-pop"),
+      $thisPop = $(`.popup[data-pop="${thisId}"]`);
+    $html.addClass("popup-open");
+    $thisPop.addClass("active");
+    if ($thisPop.find(".dim").length < 1) {
+      $thisPop.prepend('<button type="button" class="dim">닫기</button>');
+    }
+    if ($thisPop.hasClass("following-pop")) {
+      $html.removeClass("popup-open");
+      $thisPop.attr("data-pop-id", $this.attr("data-pop-id"));
+      var thisOffsetTop = $this.offset().top,
+        thisOffsetRight = $this.offset().left + $this.outerWidth(),
+        thisOffsetBottom = $(window).innerHeight() - thisOffsetTop;
+      if ($(window).innerHeight() < thisOffsetTop + $thisPop.outerHeight()) {
+        $thisPop
+          .find(".popup-wrap")
+          .css({ left: thisOffsetRight, bottom: thisOffsetBottom });
+      } else {
+        $thisPop
+          .find(".popup-wrap")
+          .css({ left: thisOffsetRight, top: thisOffsetTop });
+      }
+    }
+  });
+
+  setTimeout(function () {
+    $(".popup.active").prepend(
+      '<button type="button" class="dim">닫기</button>'
+    );
+    $(document).on(
+      "click",
+      ".popup.active .dim,.pop-close,[data-close]",
+      function () {
+        var $this = $(this),
+          $thisPop = $this.closest("[data-pop]"),
+          thisId = $thisPop.attr("data-pop"),
+          $thisBtn = $(`[data-pop="${thisId}"]:not(.popup)`);
+        $thisPop.removeClass("active");
+        $thisPop.find(".dim").remove();
+        $thisBtn.focus();
+        $html.removeClass("popup-open");
+      }
+    );
+  }, 0);
+
+  /* 아코디언 열고 닫기*/
+  $(".qna-item .question .qna-btn").on("click", function () {
+    var $this = $(this),
+      $Title = $this.parent(".question"),
+      $Item = $Title.parent(".qna-item"),
+      $Layer = $Title.siblings(".answer"),
+      IsActive = $Item.is(".active");
+    if (!IsActive) {
+      $this.addClass("active").attr("title", "답변 닫기");
+      $Item.addClass("active");
+      $Layer.slideDown();
+    } else {
+      $this.removeClass("actvie").attr("title", "답변 열기");
+      $Item.removeClass("active");
+      $Layer.slideUp();
+    }
+  });
+
+  /* 탭 */
+  $(".tab-btn").click(function () {
+    var $this = $(this),
+      index = $this.attr("data-index"),
+      $tab = $this.closest(".tab"),
+      $tabCon = $tab.find(".tab-con"),
+      $tabConActive = $tab.find(`.tab-con[data-index="${index}"]`);
+
+    $this.siblings().removeClass("active").removeAttr("title");
+    $this.attr("title", "선택됨").addClass("active");
+    if ($this.hasClass("all")) {
+      $tabCon.addClass("active").attr("title", "선택됨");
+    } else {
+      $tabCon.removeClass("active").removeAttr("title");
+      $tabConActive.addClass("active").attr("title", "선택됨");
+    }
+  });
+
+  /* table */
+  var setResponsiveTable = function () {
+    var $tableWrap = $(".table-wrap");
+    $tableWrap.each(function () {
+      var $this = $(this),
+        $thisTable = $this.find(".table"),
+        thisBreakPoint = $this.attr("data-break"),
+        thisTableWidth = $this.attr("data-width");
+      if (thisBreakPoint === "xs") {
+        if ($window.outerWidth() <= 800) {
+          console.log(thisTableWidth);
+          $thisTable.css("minWidth", thisTableWidth + "px");
+        } else {
+          $thisTable.removeAttr("style");
+        }
+      } else if (thisBreakPoint === "sm") {
+        if ($window.outerWidth <= 1000) {
+          $thisTable.css("minWidth", thisTableWidth);
+        } else {
+          $thisTable.removeAttr("style");
+        }
+      }
+    });
+  };
+  setResponsiveTable();
+  $window.on("resize", function () {
+    setResponsiveTable();
+  });
+
+  //box
+  $(".card-wrap").each(function () {
+    var $this = $(this),
+      thisTitleWidth = $this.attr("data-title-width"),
+      $thisList = $this.find(".card-list");
+    $thisItem = $this.find(".card-item");
+    $thisItem.find(".title").css("width", thisTitleWidth + "px");
+    $thisItem.find(".con").css("width", `calc(100% - ${thisTitleWidth}px`);
+  });
+
+  //테이블.responsive 반응형
+  /*$("table.table.responsive").each(function () {
+    var RowSpanExist = $(this).find("td, th").is("[rowspan]"),
+      TheadExist = $(this).find("thead").length;
+    if (RowSpanExist == false && TheadExist != 0) {
+      //rowspan이 없을 경우만 실행 (rowspan이 있으면 지원불가)
+      $(this)
+        .children("tbody")
+        .children("tr")
+        .find("th, td")
+        .each(function () {
+          var ThisIndex = $(this).index(),
+            TheadText = $(this)
+              .parents("tbody")
+              .siblings("thead")
+              .find("th")
+              .eq(ThisIndex)
+              .text();
+          $(this).attr("data-content", TheadText);
+        });
+      $(this)
+        .children("tfoot")
+        .children("tr")
+        .find("th, td")
+        .each(function () {
+          var ThisIndex = $(this).index(),
+            TheadText = $(this)
+              .parents("tfoot")
+              .siblings("thead")
+              .find("th")
+              .eq(ThisIndex)
+              .text();
+          $(this).attr("data-content", TheadText);
+        });
+    }
+  });*/
+
+  //ol.num
+  /*$("ol.num").each(function () {
+    var $this = $(this),
+      $ThisLi = $this.find(">li");
+    $ThisLi.each(function () {
+      var $this = $(this),
+        ThisIndex = $this.index() + 1;
+      $this.prepend('<i class="count">' + ThisIndex + "</i>");
+    });
+  });*/
+
+  //img mobile - 모바일에서 확대 아이콘 추가
+  /*var $imgMobile = $(".img-mobile");
+  $(window).on("load resize", function () {
+    $imgMobile.each(function () {
+      var $this = $(this),
+        $img = $this.find("img"),
+        imgRight = $img.outerWidth() + $img.offset().left;
+      $this.find(".zoom").remove();
+      if ($(window).width() < 1000) {
+        var $elem = $(
+          '<a href="' +
+            $img.attr("src") +
+            '" class="zoom" target="-blank" title="이미지확대보기 새창"></a>'
+        );
+        $this.append($img).append($elem);
+        var $zoom = $this.find(".zoom");
+        $zoom.css("left", imgRight - $this.offset().left - $zoom.width());
+      }
+    });
+  });*/
+
+  //셀렉트박스 디자인
+  /*  var $select = $container.find(".select-box"),
+    $selectAllButton = $select.find("button", "a"),
+    $selectAnchor = $select.find(".select-anchor"),
+    $selectList = $select.find(".search-list");
+
+  $selectAnchor.click(function () {
+    var $this = $(this),
+      selectButtonText = $this.text();
+
+    $this
+      .attr("title", "선택됨")
+      .closest(".select-item")
+      .addClass("active")
+      .siblings(".select-item")
+      .removeClass("active")
+      .find(".select-anchor")
+      .removeAttr("title");
+    $this.closest(".select-box").find(".search-select").text(selectButtonText);
+    $selectList.addClass("active").siblings().removeClass("active");
+  });
+
+  $selectAllButton.click(function () {
+    var $this = $(this),
+      $parentmenu = $this.parents(".select-box"),
+      IsActive = $parentmenu.is(".active");
+    if (!IsActive) {
+      $this.attr("title", "열림");
+      $parentmenu.find(".search-list").stop().slideDown("250", "easeOutExpo");
+      $parentmenu.addClass("active");
+    } else {
+      $this.removeAttr("title");
+      $parentmenu.find(".search-list").stop().slideUp("250", "easeOutExpo");
+      $parentmenu.removeClass("active");
+    }
+  });*/
+
+  // password input
+  /*$(".password .show-hide").on("click", function () {
+    var $this = $(this),
+      $thisInput = $this.siblings("input"),
+      $thisParent = $this.closest(".password");
+    $thisParent.toggleClass("show");
+    if ($thisParent.hasClass("show")) {
+      $thisInput.attr("type", "text");
+    } else {
+      $thisInput.attr("type", "password");
+    }
+  });*/
+
+  //datepicker
+  $(".input.date>input").datepicker({
+    dateFormat: "yy-mm-dd",
+    showOn: "both",
+    buttonImageOnly: false,
+    buttonText: "선택",
+    showMonthAfterYear: true,
+    monthNamesShort: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+    ],
+    monthNames: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    yearSuffix: "년",
+    dayNamesMin: ["일", "월", "화", "수", "목", "금", "토", "일"],
+    dayNames: ["일", "월", "화", "수", "목", "금", "토", "일"],
+  });
+});
