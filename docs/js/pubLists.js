3b3bc730a244b4554754195ef93931eeae2911ec
finetonight <finetonight@gmail.com>
2025-03-17 17:05:04 +0900

    수정사항 처리

--- docs/js/pubLists.js
+++ docs/js/pubLists.js
@@ -1692,20 +1692,20 @@ var PubLists = [
         complete: "대기", //"대기 | 진행중 | 완료"
         completeDate: "2025-01-00",
         etc: [""],
-      },
+      },*/
       {
-        id: "F-ABO-01-01-00-00",
+        id: "F_ABO_01_01_00_00",
         breadcrumb: "Misto > We are Misto",
         type: "page",
         design: "대기", //"대기 | 진행중 | 완료 | 없음"
         designDate: "2025-01-00",
         designAnchor: "",
         address: "/",
-        pubAddress: "./pages/eng/",
+        pubAddress: "./pages/eng/ABO_01_01_00_00.html",
         complete: "대기", //"대기 | 진행중 | 완료"
         completeDate: "2025-01-00",
         etc: [""],
-      },*/
+      },
       {
         id: "F_ABO_01_02_00_00",
         breadcrumb: "Misto > History",
