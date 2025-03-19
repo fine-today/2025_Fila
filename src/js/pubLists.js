1823577513c21fa8c54b26c6d4e31d2369ac0366
finetonight <finetonight@gmail.com>
2025-03-19 09:29:43 +0900

    we are misto

--- src/js/pubLists.js
+++ src/js/pubLists.js
@@ -986,20 +986,20 @@ var PubLists = [
         complete: "대기", //"대기 | 진행중 | 완료"
         completeDate: "2025-01-00",
         etc: [""],
-      },
+      },*/
       {
         id: "F_ABO_01_01_00_00",
         breadcrumb: "Misto > We are Misto",
         type: "page",
-        design: "대기", //"대기 | 진행중 | 완료 | 없음"
+        design: "완료", //"대기 | 진행중 | 완료 | 없음"
         designDate: "2025-01-00",
         designAnchor: "",
         address: "/",
-        pubAddress: "./pages/service/",
-        complete: "대기", //"대기 | 진행중 | 완료"
+        pubAddress: "./pages/service/ABO_01_01_00_00.html",
+        complete: "완료", //"대기 | 진행중 | 완료"
         completeDate: "2025-01-00",
         etc: [""],
-      },*/
+      },
       {
         id: "F_ABO_01_02_00_00",
         breadcrumb: "Misto > 연혁",
